ac844864f7b16b4b80d1fd4ec54255e53e427f64
Chuy <Jvelez13Soto@outlook.com>
2022-09-05 23:28:01 -0500

    color styles & fonts

--- App.js
+++ App.js
@@ -1,57 +1,29 @@
-const App = document.querySelector('#App');
+const App = document.querySelector("#App");
+let BookAdd,
+  BookDel,
+  NoteAdd,
+  NoteDel,
+  Search,
+  download,
+  upload,
+  Config,
+  Theme,
+  Users;
 
-function Init(){
-    App.innerHTML += `
-    <h1 class=" FF-O Text-Centered Es-Fs3"> Your Notebook</h1>
-    
-    <div class="ContainerP Ees-flex M-Cl Ees-Rw">
-    <div class="ContainerC ">
-      <div class="Menu M-flex M-gp2 M-f-Center">
-        <div class="Menu-Comp M-flex M-gp0 M-f-Center M-Cl" id="addN">
-          <h1 class="Menu-Ti FF-O Es-Fs1 ">Notebook</h1>
-          <div class="Menu-It M-flex M-gp1 M-f-Center">
-            <i class="bi bi-plus Ees-Fs1"></i>
-            <i class="bi bi-trash3-fill Ees-Fs1"></i>
-          </div>
-        </div>
-  
-        <div class="Menu-Comp M-flex M-gp0 M-f-Center M-Cl" id="addN">
-          <h1 class="Menu-Ti FF-O Es-Fs1 ">Note</h1>
-          <div class="Menu-It M-flex M-gp1 M-f-Center">
-            <i class="bi bi-plus Ees-Fs1"></i>
-            <i class="bi bi-trash3-fill Ees-Fs1"></i>
-          </div>
-        </div>
-  
-        <div class="Menu-Comp M-flex M-gp0 M-f-Center M-Cl">
-          <h1 class="Menu-Ti FF-O Es-Fs1">Search</h1>
-          <div class="Menu-It M-flex M-gp1 M-f-Center">
-            <i class="bi bi-search Ees-Fs1"></i>
-          </div>
-        </div>
-  
-        <div class="Menu-Comp M-flex M-gp0 M-f-Center M-Cl">
-          <h1 class="Menu-Ti FF-O Es-Fs1">File</h1>
-          <div class="Menu-It M-flex M-gp1 M-f-Center">
-            <i class="bi bi-download Ees-Fs1"></i>
-            <i class="bi bi-upload Ees-Fs1"></i>
-          </div>
-        </div>
-        <div class="Menu-Comp M-flex M-gp0 M-f-Center M-Cl">
-          <h1 class="Menu-Ti FF-O Es-Fs1">Config</h1>
-          <div class="Menu-It M-flex M-gp1 M-f-Center">
-            <i class="bi bi-gear-fill Ees-Fs1"></i>
-            <i class="bi bi-moon-fill Ees-Fs1"></i>
-            <i class="bi bi-person-fill Ees-Fs1"></i>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="ContainerC">
-      
-    </div>
-  </div>
+function Init() {
+  App.innerHTML += `
+
 `;
+  BookAdd = document.querySelector("#Badd");
+  BookDel = document.querySelector("#Bdel");
+  NoteAdd = document.querySelector("#Nadd");
+  NoteDel = document.querySelector("#Ndel");
+  Search = document.querySelector("#Search");
+  download = document.querySelector("#DownF");
+  upload = document.querySelector("#UpF");
+  Config = document.querySelector("#Config");
+  Theme = document.querySelector("#Theme");
+  Users = document.querySelector("#User");
 }
 
 Init();
